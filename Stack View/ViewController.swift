3b507615e396ea11a559e3ce09723c9d0a0003b1
Adriano Kaito <adrianokaito@Adrianos-MacBook-Pro.local>
2018-12-03 21:46:14 -0200

    commit

---
+++ Stack View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stack View
+//
+//  Created by Adriano Kaito on 27/11/18.
+//  Copyright © 2018 AK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
